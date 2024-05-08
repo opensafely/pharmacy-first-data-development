fdbb21a9d33d307715a5018933f46bba44695843
Milan Wiedemann <milan.wiedemann@gmail.com>
2024-05-08 12:46:17 +0100

    Combine dataset definition for curation

--- analysis/dataset_definition_curation.py
+++ analysis/dataset_definition_curation.py
@@ -1,5 +1,5 @@
 from ehrql import create_dataset, months, days
-from ehrql.tables.tpp import patients
+from ehrql.tables.tpp import patients, clinical_events
 from ehrql.tables.raw.tpp import medications
 
 from datetime import date
@@ -7,12 +7,25 @@ from datetime import date
 dataset = create_dataset()
 dataset.define_population(patients.exists_for_patient())
 
+# Dictionary of pharmacy first codes
+pharmacy_first_codes = {
+    # Community Pharmacy (CP) Blood Pressure (BP) Check Service (procedure)
+    "comm_pharm_bp_service": ["1659111000000107"],
+    # Community Pharmacy (CP) Contraception Service (procedure)
+    "comm_pharm_contraception_service": ["1659121000000101"],
+    # Community Pharmacist (CP) Consultation Service for minor illness (procedure)
+    "comm_pharm_consultation_service": ["1577041000000109"],
+    # Pharmacy First service (qualifier value)
+    "pharm_first_service": ["983341000000102"],
+}
+
 # https://www.england.nhs.uk/primary-care/pharmacy/pharmacy-first/
 pharmacy_first_launch_date = date(2024, 1, 31)
 
 # Time interval for selecting medications pre and post Pharmacy First launch date
 time_interval = months(3)
 
+# Select medications
 selected_medications_pre = medications.where(
     medications.date.is_on_or_between(
         pharmacy_first_launch_date - time_interval, pharmacy_first_launch_date - days(1)
@@ -30,6 +43,25 @@ selected_medications_dict = {
     "post": selected_medications_post,
 }
 
+# Select clinical events
+selected_events_pre = clinical_events.where(
+    clinical_events.date.is_on_or_between(
+        pharmacy_first_launch_date - time_interval, pharmacy_first_launch_date - days(1)
+    )
+).sort_by(clinical_events.date)
+
+selected_events_post = clinical_events.where(
+    clinical_events.date.is_on_or_between(
+        pharmacy_first_launch_date, pharmacy_first_launch_date + time_interval
+    )
+).sort_by(clinical_events.date)
+
+selected_events_dict = {
+    "pre": selected_events_pre,
+    "post": selected_events_post,
+}
+
+# Add dataset variables from medications table
 for time_interval_desc, selected_medications in selected_medications_dict.items():
     first_dmd_code_query = selected_medications.first_for_patient().dmd_code
     dataset.add_column(f"{time_interval_desc}_first_dmd_code", first_dmd_code_query)
@@ -39,3 +71,11 @@ for time_interval_desc, selected_medications in selected_medications_dict.items(
     dataset.add_column(
         f"{time_interval_desc}_first_medication_status", first_med_status_query
     )
+
+# Add dataset variables from clinical_events table
+for code_desc, code in pharmacy_first_codes.items():
+    for time_interval_desc, selected_events in selected_events_dict.items():
+        count_codes_query = selected_events.where(
+            selected_events.snomedct_code.is_in(code)
+        ).count_for_patient()
+        dataset.add_column(f"{time_interval_desc}_count_{code_desc}", count_codes_query)
--- analysis/test_dataset_definition_curation.py
+++ analysis/test_dataset_definition_curation.py
@@ -5,6 +5,7 @@ test_data = {
     # Expected in population with matching medication
     1: {
         "patients": {"date_of_birth": date(1950, 1, 1)},
+        "clinical_events": [],
         "medications_raw": [
             {
                 # First before Pharmacy first launch
@@ -35,6 +36,7 @@ test_data = {
     },
     2: {
         "patients": {"date_of_birth": date(1950, 1, 1)},
+        "clinical_events": [],
         "medications_raw": [
             {
                 # First after Pharmacy first launch
@@ -55,6 +57,72 @@ test_data = {
             "post_first_dmd_code": "39113611000001102",
             "pre_first_medication_status": None,
             "post_first_medication_status": 11,
+        },
+    },
+    # Expected in population with matching medication
+    3: {
+        "patients": {"date_of_birth": date(1950, 1, 1)},
+        "clinical_events": [
+            {
+                # Community Pharmacy (CP) Blood Pressure (BP) Check Service
+                # First before launch
+                "date": date(2024, 1, 1),
+                "snomedct_code": "1659111000000107",
+            },
+            {
+                # Community Pharmacy (CP) Blood Pressure (BP) Check Service
+                # First after launch
+                "date": date(2024, 2, 1),
+                "snomedct_code": "1659111000000107",
+            },
+            {
+                # Community Pharmacy (CP) Contraception Service (procedure)
+                # First after launch
+                "date": date(2024, 3, 1),
+                "snomedct_code": "1659121000000101",
+            },
+            {
+                # Community Pharmacist (CP) Consultation Service for minor illness (procedure)
+                # First after launch
+                "date": date(2024, 4, 1),
+                "snomedct_code": "1577041000000109",
+            },
+            {
+                # Pharmacy First service
+                # First before launch
+                "date": date(2023, 12, 1),
+                "snomedct_code": "983341000000102",
+            },
+            {
+                # Pharmacy First service
+                # First after launch
+                "date": date(2024, 4, 1),
+                "snomedct_code": "983341000000102",
+            },
+            {
+                # Pharmacy First service
+                # Second after launch, same day
+                "date": date(2024, 4, 1),
+                "snomedct_code": "983341000000102",
+            },
+            {
+                # Third Pharmacy First service
+                # Third after launch, different day
+                "date": date(2024, 4, 2),
+                "snomedct_code": "983341000000102",
+            },
+        ],
+        "medications_raw": [],
+        "expected_in_population": True,
+        "expected_columns": {
+            "pre_count_comm_pharm_bp_service": 1,
+            "pre_count_comm_pharm_contraception_service": 0,
+            "pre_count_comm_pharm_consultation_service": 0,
+            "pre_count_pharm_first_service": 1,
+            "post_count_comm_pharm_bp_service": 1,
+            "post_count_comm_pharm_contraception_service": 1,
+            "post_count_comm_pharm_consultation_service": 1,
+            "post_count_pharm_first_service": 3,
         },
     },
 }
