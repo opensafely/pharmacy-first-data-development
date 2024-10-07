f8d877dd533955529884d87a09ae5d1d70204e55
Milan Wiedemann <milan.wiedemann@gmail.com>
2024-10-07 10:30:16 +0100

    Add PF clinical and pathway events
    This dataset definition now only covers clinical events and pathways, no longer medication status. Medication status is investivated in it's own dataset definition now.

--- analysis/dataset_definition_pf_data_development.py
+++ analysis/dataset_definition_pf_data_development.py
@@ -4,7 +4,7 @@ from ehrql.tables.raw.tpp import medications
 
 from datetime import date
 
-from codelists import *
+from codelists import pharmacy_first_clinical_pathways_cod
 
 dataset = create_dataset()
 dataset.configure_dummy_data(population_size=1000)
@@ -27,19 +27,9 @@ pharmacy_first_event_codes = [
     code for codelist in pharmacy_first_events_dict.values() for code in codelist
 ]
 
-pharmacy_first_med_codes = (
-    acute_otitis_media_tx_cod
-    + impetigo_treatment_tx_cod
-    + infected_insect_bites_tx_cod
-    + shingles_treatment_tx_cod
-    + sinusitis_tx_cod
-    + sore_throat_tx_cod
-    + urinary_tract_infection_tx_cod
-)
-
 # https://www.england.nhs.uk/primary-care/pharmacy/pharmacy-first/
 # For further projects we should review if choosing the actual start date
-# will give us the most accurate cut-off in OpenSAFELY 
+# will give us the most accurate cut-off in OpenSAFELY
 pharmacy_first_launch_date = date(2024, 2, 1)
 
 # Time interval for selecting medications pre and post Pharmacy First launch date
@@ -49,63 +39,52 @@ pharmacy_first_ids = clinical_events.where(
     clinical_events.snomedct_code.is_in(pharmacy_first_event_codes)
 ).consultation_id
 
+pharmacy_first_dates = clinical_events.where(
+    clinical_events.snomedct_code.is_in(pharmacy_first_event_codes)
+).date
+
 # Select clinical events pre and post PF launch date
-selected_events_pre = (
-    clinical_events.where(
-        clinical_events.date.is_on_or_between(
-            pharmacy_first_launch_date - time_interval,
-            pharmacy_first_launch_date - days(1),
-        )
+selected_all_events_pre = clinical_events.where(
+    clinical_events.date.is_on_or_between(
+        pharmacy_first_launch_date - time_interval,
+        pharmacy_first_launch_date - days(1),
     )
-    .where(clinical_events.consultation_id.is_in(pharmacy_first_ids))
-    .sort_by(clinical_events.date)
+).sort_by(clinical_events.date)
+
+selected_pfid_events_pre = selected_all_events_pre.where(
+    clinical_events.consultation_id.is_in(pharmacy_first_ids)
 )
 
-selected_events_post = (
-    clinical_events.where(
-        clinical_events.date.is_on_or_between(
-            pharmacy_first_launch_date, pharmacy_first_launch_date + time_interval
-        )
-    )
-    .where(clinical_events.consultation_id.is_in(pharmacy_first_ids))
-    .sort_by(clinical_events.date)
+selected_pfdate_events_pre = selected_all_events_pre.where(
+    clinical_events.date.is_in(pharmacy_first_dates)
 )
+
+selected_all_events_post = clinical_events.where(
+    clinical_events.date.is_on_or_between(
+        pharmacy_first_launch_date, pharmacy_first_launch_date + time_interval
+    )
+).sort_by(clinical_events.date)
+
+selected_pfid_events_post = selected_all_events_post.where(
+    clinical_events.consultation_id.is_in(pharmacy_first_ids)
+)
+
+selected_pfdate_events_post = selected_all_events_post.where(
+    clinical_events.date.is_in(pharmacy_first_dates)
+    )
 
 selected_events_dict = {
-    "pre": selected_events_pre,
-    "post": selected_events_post,
+    "pre_all": selected_all_events_pre,
+    "pre_pfid": selected_pfid_events_pre,
+    "pre_pfdate": selected_pfdate_events_pre,
+    "post_all": selected_all_events_post,
+    "post_pfid": selected_pfid_events_post,
+    "post_pfdate": selected_pfdate_events_post,
 }
 
-# Select medications pre and post PF launch date
-# This only selects medications that are:
-# (1) part of a PF codelist (pharmacy_first_med_codes)
-# (2) linked to a PF consultation (pharmacy_first_ids)
-selected_medications_pre = (
-    medications.where(
-        medications.date.is_on_or_between(
-            pharmacy_first_launch_date - time_interval,
-            pharmacy_first_launch_date - days(1),
-        )
-    )
-    .where(medications.dmd_code.is_in(pharmacy_first_med_codes))
-    .where(medications.consultation_id.is_in(pharmacy_first_ids))
-    .sort_by(medications.date)
-)
-
-selected_medications_post = (
-    medications.where(
-        medications.date.is_on_or_between(
-            pharmacy_first_launch_date, pharmacy_first_launch_date + time_interval
-        )
-    )
-    .where(medications.dmd_code.is_in(pharmacy_first_med_codes))
-    .where(medications.consultation_id.is_in(pharmacy_first_ids))
-    .sort_by(medications.date)
-)
-
-selected_medications_dict = {
-    "pre": selected_medications_pre,
-    "post": selected_medications_post,
+pf_codelists_dict = {
+    "pf_events": pharmacy_first_event_codes,
+    "pf_pathways": pharmacy_first_clinical_pathways_cod,
 }
 
 # Count distinct pharmacy first consultations per patient
@@ -114,21 +93,12 @@ for time_interval_desc, selected_events in selected_events_dict.items():
         selected_events.consultation_id.count_distinct_for_patient()
     )
     dataset.add_column(
-        f"{time_interval_desc}_count_distinct_consultations",
+        f"{time_interval_desc}_count_distinct_pfids",
         count_distinct_consultations_query,
     )
 
-# Get first medication status and dmd_code linked to pharmacy first consultation
-for time_interval_desc, selected_medications in selected_medications_dict.items():
-    first_dmd_code_query = selected_medications.first_for_patient().dmd_code
-    first_med_status_query = selected_medications.first_for_patient().medication_status
-    dataset.add_column(f"{time_interval_desc}_first_dmd_code", first_dmd_code_query)
-    dataset.add_column(
-        f"{time_interval_desc}_first_medication_status", first_med_status_query
-    )
-
 # Count pharmacy first codes
-for code_desc, code in pharmacy_first_events_dict.items():
+for code_desc, code in pf_codelists_dict.items():
     for time_interval_desc, selected_events in selected_events_dict.items():
         count_codes_query = selected_events.where(
             selected_events.snomedct_code.is_in(code)
