17aa5ad495b8d7c906af74987d5cfcabedfc3989
Viveck <viveck.kingsley@gmail.com>
2024-11-28 16:03:27 +0000

    Add PF meds codelists to codelists.py

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -39,3 +39,21 @@ pharmacy_first_clinical_pathways_cod = codelist_from_csv(
     "codelists/opensafely-pharmacy-first-clinical-pathway-conditions.csv",
     column="code",
 )
+
+# Combine all medication codelists
+pharmacy_first_med_codes = (
+    acute_otitis_media_tx_cod
+    + impetigo_treatment_tx_cod
+    + infected_insect_bites_tx_cod
+    + shingles_treatment_tx_cod
+    + sinusitis_tx_cod
+    + sore_throat_tx_cod
+    + urinary_tract_infection_tx_cod
+)
+
+pharmacy_first_event_codes = [
+    # Community Pharmacist (CP) Consultation Service for minor illness (procedure)
+    "1577041000000109",
+    # Pharmacy First service (qualifier value)
+    "983341000000102",
+]
