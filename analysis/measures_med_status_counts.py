944414c703c386483b5885416bd02f80e9562d9b
viv3ckj <viveck.kingsley@gmail.com>
2026-04-24 12:58:23 +0100

    Test out new measures which counts meds supplied by medication status

---
+++ analysis/measures_med_status_counts.py
@@ -0,0 +1,20 @@
+from ehrql import create_dataset, months, days, INTERVAL, create_measures
+from ehrql.tables.tpp import patients, clinical_events
+from ehrql.tables.raw.tpp import medications
+
+from codelists import pharmacy_first_event_codes
+measures = create_measures()
+start_date = "2023-08-01"
+monthly_intervals = 32
+
+pharmacy_first_ids = clinical_events.where(clinical_events.snomedct_code.is_in(pharmacy_first_event_codes)).consultation_id
+
+pf_medications = medications.where(medications.consultation_id.is_in(pharmacy_first_ids)).where(medications.date.is_on_or_between(INTERVAL.start_date, INTERVAL.end_date))
+
+measures.define_measure(
+    name="pf_medications_by_med_status",
+    numerator=pf_medications.count_for_patient(),
+    denominator=pf_medications.count_for_patient(),
+    group_by={"medication_status": pf_medications.medication_status},
+    intervals=months(monthly_intervals).starting_on(start_date),
+)
