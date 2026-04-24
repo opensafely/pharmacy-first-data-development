5e6d5f52090f08a00a6226fe0a4dc46963f05514
viv3ckj <viveck.kingsley@gmail.com>
2026-04-24 13:20:58 +0100

    Fix job error

--- analysis/measures_med_status_counts.py
+++ analysis/measures_med_status_counts.py
@@ -9,12 +9,13 @@ monthly_intervals = 32
 
 pharmacy_first_ids = clinical_events.where(clinical_events.snomedct_code.is_in(pharmacy_first_event_codes)).consultation_id
 
-pf_medications = medications.where(medications.consultation_id.is_in(pharmacy_first_ids)).where(medications.date.is_on_or_between(INTERVAL.start_date, INTERVAL.end_date))
+pf_medications = medications.where(medications.consultation_id.is_in(pharmacy_first_ids)).where(medications.date.is_on_or_between(INTERVAL.start_date, INTERVAL.end_date)).sort_by(medications.date).first_for_patient()
+medication_status = pf_medications.medication_status
 
 measures.define_measure(
     name="pf_medications_by_med_status",
     numerator=pf_medications.count_for_patient(),
     denominator=pf_medications.count_for_patient(),
-    group_by={"medication_status": pf_medications.medication_status},
+    group_by={"medication_status": medication_status},
     intervals=months(monthly_intervals).starting_on(start_date),
 )
